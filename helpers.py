82eb30ca02fc9eaae4599cc8dfcff9dd43ba37c7
xiaguangzhiw <ahw51697@163.com>
2021-03-19 08:36:21 +0800

    Add helpers functions

--- helpers.py
+++ helpers.py
@@ -4,3 +4,14 @@ def removeHTMLtags(text):
   cleanr = re.compile('<.*?>')
   cleanText = re.sub(cleanr, '', text)
   return cleanText
+
+def prepareNotes(notesRaw):
+  notes = notesRaw.split('--*/notes-separator/*--')
+  notes.pop()
+  return notes
+  
+def getLabel(keepObj, labelName):
+  label = keepObj.findLabel(labelName)
+  if label is None:
+    label = keepObj.createLabel(labelName)
+  return label
--- script.py
+++ script.py
@@ -1,27 +1,31 @@
 import osascript
 import gkeepapi
 import yaml
-from helpers import removeHTMLtags
+from helpers import removeHTMLtags, getLabel, prepareNotes
 
 with open(r'credentials.yml') as file:
-    credentials = yaml.load(file, Loader=yaml.FullLoader)
+  credentials = yaml.load(file, Loader=yaml.FullLoader)
 
-applescript = open("notes.applescript","r").read()
+applescript = open('notes.applescript','r').read()
 
-code,err,out = osascript.run(applescript)
+err,code,out = osascript.run(applescript)
 
-notesList = out.split("--*/notes-separator/*--")
-notesList.pop()
+if err:
+  raise Exception('Something wrong with reading notes')
+
+notesList = prepareNotes(out)
 
 keep = gkeepapi.Keep()
 keep.login(credentials['login'], credentials['pass'])
+
+label = getLabel(keep, "From my Mac")
 
 for note in notesList:
   noteContent = note.split("--*/inside-note-separator/*--")
   noteTitle = noteContent[0]
   noteBody = noteContent[1]
 
-  print(noteContent)
-
   gnote = keep.createNote(noteTitle, removeHTMLtags(noteBody))
-  keep.sync()
+  gnote.labels.add(label)
+
+keep.sync()
